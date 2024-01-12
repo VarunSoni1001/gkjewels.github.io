5967ae8de327138b56559992a903e9bb00f1fb29
Varun Soni <varun1001soni@gmail.com>
2024-01-12 20:31:50 +0530

    fixes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Configuration} */
+module.exports = {
+  content: [
+    './**/*.{html,htaccess}',
+    './sub/**/*.html',
+    './sub/form/**/*.html',
+    './js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
